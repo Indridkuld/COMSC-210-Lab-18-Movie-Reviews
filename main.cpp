39bda6f7dd9b043118307e1556469962aeee1196
Aidan <aidanmichaelwoodcock@gmail.com>
2025-10-03 15:55:53 -0700

    update

--- main.cpp
+++ main.cpp
@@ -5,3 +5,24 @@
 
 using namespace std;
 
+struct reviewNode
+{
+    string comment; 
+    int rating;
+    reviewNode* next;
+};
+
+// function prototypes
+void addFront(reviewNode*& head, const string& comment, int rating);
+void addTail(reviewNode*& head, const string& comment, int rating);
+
+int main () {
+reviewNode* head = nullptr;
+cout << "Which linked list method should we use?";
+cout << "\n [1] New nodes are added at the head of the linked list";
+cout << "\n [2] New nodes are added at the tail of the linked list \n" << endl;
+
+
+
+    return 0;
+}
