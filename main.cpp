747e6bbc13be9c21ac3b33509bf07f30d14f3de1
Aidan <aidanmichaelwoodcock@gmail.com>
2025-10-03 16:20:24 -0700

    update

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ reviewNode* head = nullptr;
 int choice;
 float rating;
 string comment;
+char ans;
 cout << "Which linked list method should we use?";
 cout << "\n [1] New nodes are added at the head of the linked list";
 cout << "\n [2] New nodes are added at the tail of the linked list \n" << endl;
@@ -31,15 +32,27 @@ while(choice == 1 || choice == 2) {
 cout << "Enter review rating 0-5: "; 
 cin >> rating;
 cin.ignore();
+cout << "Enter review comment: ";
+getline(cin, comment);
+cin.ignore();
     if(choice == 1) {
 
-    }
-}else if 
-    cout << "Invalid choice. Please enter 1 or 2: ";
-    cin >> choice;
-    cin.ignore();
+    }else if(choice == 2) {
 
-    return 0;
+    }
+cout << "Enter another review? Y/N: ";
+cin >> ans;
+cin.ignore();
+if(ans == 'N' || ans == 'n') {
+    break;  
+}else if(ans == 'Y' || ans == 'y') {
+    continue; 
+    }
+}
+
+
+return 0;
+
 }
 // function definitions
 void addFront(reviewNode*& head, const string& comment, int rating) {
