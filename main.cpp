5212fccdb99ac12a3469d1ddaafa6ee7f4dd419f
Aidan <aidanmichaelwoodcock@gmail.com>
2025-10-03 16:31:45 -0700

    update

--- main.cpp
+++ main.cpp
@@ -28,31 +28,51 @@ cout << "\n [2] New nodes are added at the tail of the linked list \n" << endl;
 cin >> choice;
 cin.ignore(); 
 cout << "Choice: " << choice << endl;
-while(choice == 1 || choice == 2) {
-cout << "Enter review rating 0-5: "; 
-cin >> rating;
-cin.ignore();
-cout << "Enter review comment: ";
-getline(cin, comment);
-cin.ignore();
-    if(choice == 1) {
-
-    }else if(choice == 2) {
-
-    }
-cout << "Enter another review? Y/N: ";
-cin >> ans;
-cin.ignore();
-if(ans == 'N' || ans == 'n') {
-    break;  
-}else if(ans == 'Y' || ans == 'y') {
-    continue; 
-    }
+if(choice != 1 && choice != 2) {
+    cout << "Invalid choice. Please enter 1 or 2: ";
+    cin >> choice;
+    cin.ignore();
 }
+while(choice == 1 || choice == 2) {
+    cout << "Enter review rating 0-5: "; 
+    cin >> rating;
+    cin.ignore();
+    cout << "Enter review comment: ";
+    getline(cin, comment);
+    cin.ignore();
+        if(choice == 1) {
+            addFront(head, comment, rating);
+        }else if(choice == 2) {
+            addTail(head, comment, rating);
+        }
+    cout << "Enter another review? Y/N: ";
+    cin >> ans;
+    cin.ignore();
+    if(ans == 'N' || ans == 'n') {
+        break;  
+    }else if(ans == 'Y' || ans == 'y') {
+        continue; 
+        }
+}
+cout << "\nOutputting all reviews:\n" << endl;
+reviewNode* current = head;
+while(current) {
+    cout << "Rating: " << current->rating << endl;
+    cout << "Comment: " << current->comment << endl;
+    cout << "Average: " << (current->rating / 5) * 100 << endl;
 
+    current = current->next;
+}
+// Free allocated memory
+current = head;
+while (current) {
+    reviewNode* temp = current;
+    current = current->next;
+    delete temp;
+}
+head = nullptr;
 
 return 0;
-
 }
 // function definitions
 void addFront(reviewNode*& head, const string& comment, int rating) {
